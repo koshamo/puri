e0669997df6bfaadbadb7e36b4b3dc23decd2e1d
koshamo <jochen.rassler@gmail.com>
2020-01-12 11:11:42 +0100

    added offset to display small ships on widget top

--- src/com/github/koshamo/puri/ui/controls/board/GoodsShipSkin.java
+++ src/com/github/koshamo/puri/ui/controls/board/GoodsShipSkin.java
@@ -17,6 +17,7 @@ public class GoodsShipSkin extends SkinBase<GoodsShip> {
 	private final int places;
 	private final int upperRowCnt;
 	private final int lowerRowCnt;
+	private final int smallShipOffset;
 	
 	protected GoodsShipSkin(GoodsShip control, int places) {
 		super(control);
@@ -24,9 +25,11 @@ public class GoodsShipSkin extends SkinBase<GoodsShip> {
 		if (places > 4) {
 			upperRowCnt = places / 2;
 			lowerRowCnt = places / 2 + places % 2;
+			smallShipOffset = 0;
 		} else {
 			upperRowCnt = 0;
 			lowerRowCnt = places;
+			smallShipOffset = 25;
 		}
 		drawComponent(PlantationType.NONE, 0);
 	}
@@ -43,16 +46,16 @@ public class GoodsShipSkin extends SkinBase<GoodsShip> {
 		this.getChildren().add(pane);
 	}
 
-	private static void drawShip(Pane pane) {
+	private void drawShip(Pane pane) {
 		Path path = new Path();
 
-		MoveTo start = new MoveTo(1, 50);
-		LineTo line1 = new LineTo(21, 55);
-		LineTo line2 = new LineTo(121, 55);
-		LineTo line3 = new LineTo(141, 50);
-		LineTo line4 = new LineTo(121, 85);
-		LineTo line5 = new LineTo(21, 85);
-		LineTo line6 = new LineTo(1, 50);
+		MoveTo start = new MoveTo(1, 50 - smallShipOffset);
+		LineTo line1 = new LineTo(21, 55 - smallShipOffset);
+		LineTo line2 = new LineTo(121, 55 - smallShipOffset);
+		LineTo line3 = new LineTo(141, 50 - smallShipOffset);
+		LineTo line4 = new LineTo(121, 85 - smallShipOffset);
+		LineTo line5 = new LineTo(21, 85 - smallShipOffset);
+		LineTo line6 = new LineTo(1, 50 - smallShipOffset);
 
 		path.getElements().add(start);
 		path.getElements().addAll(line1, line2, line3, line4, line5, line6);
@@ -63,7 +66,7 @@ public class GoodsShipSkin extends SkinBase<GoodsShip> {
 	private void drawText(Pane pane, int amount) {
 		Text text = new Text(amount + " / " + places);
 		text.setX(60);
-		text.setY(75);
+		text.setY(75 - smallShipOffset);
 		pane.getChildren().add(text);
 	}
 	
@@ -80,9 +83,9 @@ public class GoodsShipSkin extends SkinBase<GoodsShip> {
 			else 
 				curColor = PrColors.DEFAULT_BGD.getColor();
 			Rectangle border = new Rectangle(22, 22, Color.BLACK);
-			border.relocate(21+i*distance, 30);
+			border.relocate(21+i*distance, 30 - smallShipOffset);
 			Rectangle rect = new Rectangle(20, 20, curColor);
-			rect.relocate(22+i*distance, 31);
+			rect.relocate(22+i*distance, 31 - smallShipOffset);
 			pane.getChildren().addAll(border, rect);
 		}
 		for (int i = 0; i < upperRowCnt; i++, load++) {
