1404c8588ff76c4f855bd3cc74a044f3265b6e1c
koshamo <jochen.rassler@gmail.com>
2020-01-12 11:07:13 +0100

    added ship size constants

--- src/com/github/koshamo/puri/setup/StartupConstants.java
+++ src/com/github/koshamo/puri/setup/StartupConstants.java
@@ -6,10 +6,18 @@ public final class StartupConstants {
 	public final int COLONISTS;
 	public final int MIN_COLONISTS;
 	public final int MIN_PLANTATION;
+	public final int SMALL_SHIP_PLACES;
+	public final int MEDIUM_SHIP_PLACES;
+	public final int LARGE_SHIP_PLACES;
+	
 	
 	public StartupConstants(int players) {
 		MIN_COLONISTS = players;
 		MIN_PLANTATION = players + 1;
+		
+		SMALL_SHIP_PLACES = players + 1;
+		MEDIUM_SHIP_PLACES = players + 2;
+		LARGE_SHIP_PLACES = players + 3;
 		
 		if (players == 3) {
 			VICTORY_POINTS = 75;
