d6c15b8a7838c150b49511a3e66fdc1ff3336fa2
koshamo <jochen.rassler@gmail.com>
2020-01-27 10:51:11 +0100

    implemented boards plantation initial setup

--- src/com/github/koshamo/puri/ui/controls/board/BoardPlantation.java
+++ src/com/github/koshamo/puri/ui/controls/board/BoardPlantation.java
@@ -1,6 +1,10 @@
 package com.github.koshamo.puri.ui.controls.board;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.github.koshamo.puri.setup.PlantationType;
+import com.github.koshamo.puri.utils.ListUtils;
 
 import javafx.scene.control.Control;
 import javafx.scene.control.Skin;
@@ -9,18 +13,47 @@ import javafx.scene.control.Skin;
 
 	private final BoardPlantationSkin skin;
 	
+	private final int numPlayers;
+	private List<PlantationType> plantations;
+	private final int quarries;
+	
 	public BoardPlantation(int numPlayers) {
+		this.numPlayers = numPlayers;
+		quarries = PlantationType.QUARRY.getMax();
 		skin = new BoardPlantationSkin(this, numPlayers);
+		initPlantations();
 		update();
 	}
 	
+	private void initPlantations() {
+		plantations = new LinkedList<>();
+		int usedCorn = numPlayers / 2;
+		int usedIndigo = numPlayers - usedCorn;
+		
+		for (int indigo = 0; indigo < PlantationType.INDIGO.getMax() - usedIndigo; indigo++)
+			plantations.add(PlantationType.INDIGO);
+		for (int sugar = 0; sugar < PlantationType.SUGAR.getMax(); sugar++)
+			plantations.add(PlantationType.SUGAR);
+		for (int corn = 0; corn < PlantationType.CORN.getMax() - usedCorn; corn++)
+			plantations.add(PlantationType.CORN);
+		for (int tobacco = 0; tobacco < PlantationType.TOBACCO.getMax(); tobacco++)
+			plantations.add(PlantationType.TOBACCO);
+		for (int coffee = 0; coffee < PlantationType.COFFEE.getMax(); coffee++)
+			plantations.add(PlantationType.COFFEE);
+		
+		plantations = ListUtils.generateRandomList(plantations);
+	}
+
 	@Override
 	protected Skin<?> createDefaultSkin() {
 		return skin;
 	}
 	
 	public void update() {
-		// TODO: use class internal fields to store data and use them here
-		skin.drawComponent(50,3, new PlantationType[] {PlantationType.CORN, PlantationType.NONE, PlantationType.TOBACCO, PlantationType.INDIGO});
+		PlantationType[] toDraw = new PlantationType[numPlayers + 1];
+		for (int i = 0; i < toDraw.length; i++) {
+			toDraw[i] = plantations.remove(0);
+		}
+		skin.drawComponent(plantations.size(), quarries, toDraw);
 	}
 }
