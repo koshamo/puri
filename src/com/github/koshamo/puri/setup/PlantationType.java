be0c23a0bd6d5d21680f052535fdd4776e078524
koshamo <jochen.rassler@gmail.com>
2020-02-19 21:51:23 +0100

    added trading price to plantaation type

--- src/com/github/koshamo/puri/setup/PlantationType.java
+++ src/com/github/koshamo/puri/setup/PlantationType.java
@@ -8,16 +8,28 @@ package com.github.koshamo.puri.setup;
 
 public enum PlantationType {
 
-	QUARRY (8), INDIGO (12), SUGAR (11), CORN (10), TOBACCO (9), COFFEE (8), NONE (0);
+	QUARRY (8, 0), 
+	INDIGO (12, 1), 
+	SUGAR (11, 2), 
+	CORN (10, 0), 
+	TOBACCO (9, 3), 
+	COFFEE (8, 4), 
+	NONE (0, 0);
 	
 	private int max;
+	private int price;
 	
-	private PlantationType(int max) {
+	private PlantationType(int max, int price) {
 		this.max = max;
+		this.price = price;
 	}
 	
 	public int getMax() {
 		return max;
+	}
+	
+	public int getPrice() {
+		return price;
 	}
 	
 	public static PlantationType getByString(String type) {
