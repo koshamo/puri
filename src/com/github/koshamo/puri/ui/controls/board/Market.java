f36bb7b9005c968e799d0fca9a3c7383d5091257
koshamo <jochen.rassler@gmail.com>
2020-02-19 21:51:59 +0100

    added interface methods for trader actions

--- src/com/github/koshamo/puri/ui/controls/board/Market.java
+++ src/com/github/koshamo/puri/ui/controls/board/Market.java
@@ -16,6 +16,34 @@ import javafx.scene.control.Skin;
 	public Market() {
 		skin = new MarketSkin(this);
 		products = new ArrayList<>(4);
+		clearProducts();
+	}
+
+	public boolean hasEmptyPlace() {
+		if (products.get(0) == PlantationType.NONE)
+			return true;
+		return false;
+	}
+	
+	public boolean hasProduct(PlantationType type) {
+		return products.contains(type);
+	}
+	
+	public void addProduct(PlantationType type) {
+		products.remove(0);
+		products.add(type);
+		update();
+	}
+	
+	public List<PlantationType> getProducts() {
+		return products;
+	}
+	
+	public void clearProducts() {
+		products.clear();
+		for (int i = 0; i < 4; i++)
+			products.add(PlantationType.NONE);
+		update();
 	}
 	
 	@Override
